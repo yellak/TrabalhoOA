c08e9a17aed7296475057d9cbb6db5294a37a7bc
Kálley Wilkerson <yellakkalley@hotmail.com>
2018-05-25 14:15:38 -0300

    Comentários no lst_sec.h

--- lst_sec.h
+++ lst_sec.h
@@ -1,11 +1,15 @@
+/* TAD: Lista de índices secundários */
+
 #include "lst_prim_inv.h"
 
+/* Definição do tipo nó de uma lista secundária */
 typedef struct nosec {
   char chave[9];
   LstIP* lista_invertida;
   struct nosec* proximo;
 } NoSec;
 
+/* Definição da lista de índices secundários */
 typedef struct lstindsec {
   NoSec* cabeca;
 } LstIndSec;
